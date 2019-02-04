41e5e3ba9c97165ba84473fdf055f0bdc7aca3a8
Antonio500 <sudakovanton500@yandex.ru>
2019-02-04 17:00:16 +0300

    Add Animation

--- Assets/Scripts/Game/GameControl.cs
+++ Assets/Scripts/Game/GameControl.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class GameControl : MonoBehaviour
 {
@@ -6,7 +7,7 @@ public class GameControl : MonoBehaviour
     public GameObject[] listCell;
     public GameObject[,] arrayCell;
     public int emptyX, emptyY;
-    public int randNumber ;
+    public int randNumber;
     public int countClick = 0;
     public bool WinFlag = false;
     int[,] array;
@@ -28,29 +29,41 @@ public class GameControl : MonoBehaviour
         // Если да, двигаем циклично пустою ячейку до нажатой 
         if (newPosY == emptyY)
         {
-            if (newPosX == emptyX) return;
             int tempPos = emptyX;
             while (tempPos != newPosX)
             {
-                if (tempPos < newPosX) tempPos++;
-                else tempPos--;
-                SwapCell(tempPos, emptyY);
+                if (tempPos < newPosX)
+                {
+                    tempPos++;
+                    GameObject.Find(arrayCell[tempPos, emptyY].name).GetComponent<Animation>().Play("Scroll Top");
+                }
+                else
+                {
+                    tempPos--;
+                    GameObject.Find(arrayCell[tempPos, emptyY].name).GetComponent<Animation>().Play("Scroll Bot");
+                }
+                StartCoroutine(SwapCell(0.2f, tempPos, emptyY));
             }
             countClick++;
-            WinFlag = СheckСorrectness();
         }
         else if (newPosX == emptyX)
         {
-            if (newPosY == emptyY) return;
             int tempPos = emptyY;
             while (tempPos != newPosY)
             {
-                if (tempPos < newPosY) tempPos++;
-                else tempPos--;
-                SwapCell(emptyX, tempPos);
+                if (tempPos < newPosY)
+                {
+                    tempPos++;
+                    GameObject.Find(arrayCell[emptyX, tempPos].name).GetComponent<Animation>().Play("Scroll Left");
+                }
+                else
+                {
+                    tempPos--;
+                    GameObject.Find(arrayCell[emptyX, tempPos].name).GetComponent<Animation>().Play("Scroll Right");
+                }
+                StartCoroutine(SwapCell(0.2f, emptyX, tempPos));
             }
             countClick++;
-            WinFlag = СheckСorrectness();
         }
 
     }
@@ -86,18 +99,20 @@ public class GameControl : MonoBehaviour
     }
 
     //Меняет данную ячейку с пустой
-    void SwapCell(int moveX, int moveY)
+    IEnumerator SwapCell(float time, int moveX, int moveY)
     {
-        // Запоминаем позицию сдвигаемой ячейки
+        yield return new WaitForSeconds(time);
+        // Запоминаем позицию пустой ячейки
         Vector2 anchorMinTmp = arrayCell[emptyX, emptyY].GetComponent<RectTransform>().anchorMin,
                 anchorMaxTmp = arrayCell[emptyX, emptyY].GetComponent<RectTransform>().anchorMax;
-        // Передвигаем ее на место пустой
+        // Передвигаем ее на место сдвигаемой 
         arrayCell[emptyX, emptyY].GetComponent<RectTransform>().anchorMin = arrayCell[moveX, moveY].GetComponent<RectTransform>().anchorMin;
         arrayCell[emptyX, emptyY].GetComponent<RectTransform>().anchorMax = arrayCell[moveX, moveY].GetComponent<RectTransform>().anchorMax;
-        // Ставим пустую на место сдвигаемой
+        // Ставим сдвигаемeмую на место пустой
         arrayCell[moveX, moveY].GetComponent<RectTransform>().anchorMin = anchorMinTmp;
         arrayCell[moveX, moveY].GetComponent<RectTransform>().anchorMax = anchorMaxTmp;
-
+        arrayCell[moveX, moveY].GetComponent<RectTransform>().offsetMin = new Vector2(0, 0);
+        arrayCell[moveX, moveY].GetComponent<RectTransform>().offsetMax = new Vector2(0, 0);
         // Меняем местами объекты в самой таблице
         GameObject tempObject = arrayCell[emptyX, emptyY];
         arrayCell[emptyX, emptyY] = arrayCell[moveX, moveY];
@@ -111,6 +126,8 @@ public class GameControl : MonoBehaviour
         // Запоминаем место пустой ячейки
         emptyX = moveX;
         emptyY = moveY;
+
+        WinFlag = СheckСorrectness();
     }
 
     // Перемешивает массив
@@ -124,26 +141,24 @@ public class GameControl : MonoBehaviour
             {
                 newPosY = emptyY;
                 newPosX = Random.Range(0, size);
-                if (newPosX == emptyX) return;
                 int tempPos = emptyX;
                 while (tempPos != newPosX)
                 {
                     if (tempPos < newPosX) tempPos++;
                     else tempPos--;
-                    SwapCell(tempPos, emptyY);
+                    StartCoroutine(SwapCell(0f, tempPos, emptyY));
                 }
             }
             else
             {
                 newPosX = emptyX;
                 newPosY = Random.Range(0, size);
-                if (newPosY == emptyY) return;
                 int tempPos = emptyY;
                 while (tempPos != newPosY)
                 {
                     if (tempPos < newPosY) tempPos++;
                     else tempPos--;
-                    SwapCell(emptyX, tempPos);
+                    StartCoroutine(SwapCell(0f, emptyX, tempPos));
                 }
             }
         }
@@ -170,7 +185,7 @@ public class GameControl : MonoBehaviour
     {
         WinFlag = false;
         countClick = 0;
-        RandomPuzzle(randNumber);
+        RandomPuzzle(randNumber); 
         Debug.Log("New Game " + randNumber);
     }
 }
